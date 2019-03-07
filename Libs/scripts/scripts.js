168441af14e569e923c200aaa1202106af406d05
altaf Khan <altafkhan@altafs-MacBook-Pro.local>
2019-03-07 11:16:03 +1100

    fixed the bugs

--- Libs/scripts/scripts.js
+++ Libs/scripts/scripts.js
@@ -1,18 +1,29 @@
 let app 
 
 (function (app){
-let title=document.title; 
 
-console.log(title);
-
-switch(title){
-    case "ContactUs":
-    break; 
-
-
+function start(){
+    
+    let title=document.title; 
+    switch(title){
+        case "Home":
+        console.log(title);
+        break; 
+        case "AboutUs":
+        console.log(title);
+        break; 
+        case "Services":
+        console.log(title);
+        break; 
+        
+        case "ContactUs":
+        console.log(title);
+        break; 
+        
+        default:
+        break; 
+    }
 }
-
-
-
+window.addEventListener("load", start);
 
 }( app || (app={})))
